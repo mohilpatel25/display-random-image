bf2232e08128b875d6d9a7594761a0bebb20060d
Mohil Patel <48318085+mohilpatel25@users.noreply.github.com>
2020-10-24 13:47:21 +0530

    Update app.py

--- app.py
+++ app.py
@@ -5,10 +5,6 @@ import json
 app = flask.Flask(__name__)
 app.config["DEBUG"] = True
 
-class randomImage:
-	images={}
-
-randomObj = randomImage()
 
 @app.route('/', methods=['GET'])
 def home():
@@ -18,12 +14,12 @@ def home():
 
 @app.route('/<int:id>', methods=['GET'])
 def req(id):
-	if(id in randomObj.images.keys()):
+	if(id in images.keys()):
 		out = "<img src='"+str(images[id])+"'/>"
 	else:
 		response = requests.get('http://www.picsum.photos/200')
 		out = "<img src='"+str(response.url)+"'/>"
-		randomObj.images[id] = response.url
+		images[id] = response.url
 	return out
 
 @app.route('/images', methods=['GET'])
